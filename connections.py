9bdea9bcd2039babde33bb032a9efe6e078aafcd
Yona2000 <168690523+Yona2000@users.noreply.github.com>
2025-07-31 23:06:30 -0400

    Delete connections.py

--- connections.py
+++
@@ -1,7 +0,0 @@
-# Jonathan, Davila | 7/27/2025 | 9.2 Assignment APIs
-# Variables storing APIs urls for getting data
-
-
-Api_Url_Open_Notify_Astronauts = "http://api.open-notify.org/astro.json"
-
-Api_Url_Poke_Api = "https://pokeapi.co/api/v2/generation/1/" 

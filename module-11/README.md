78581d725bbc93b1a9266d7b0737fc6d106be5d0
Yona2000 <168690523+Yona2000@users.noreply.github.com>
2025-08-07 00:27:59 -0400

    Adding Module-11 Py files and Word Doc

---
+++ module-11/README.md
@@ -0,0 +1,2 @@
+Module 11: Creating a Django setup for web development for class.
+Jonathan Davila | M11.2 Assignment Django Basics
---
+++ module-11/demo/davila/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DavilaConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "davila"
